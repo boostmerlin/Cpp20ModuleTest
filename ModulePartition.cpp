e57fd90b0446615cd21d6a4fd4c81954f044e544
boostmerlin <merlinxx@email.com>
2025-03-27 16:36:31 +0800

    update code

--- ModulePartition.cpp
+++ ModulePartition.cpp
@@ -2,8 +2,8 @@ module myModule:ModulePartition; //模块声明应该放在开头
 
 import <iostream>; // 这里是可以import的
 //#include <iostream> // 也可以#include
-//import <vector>; // 为什么include 就可以，import就不行！？
-//#include <vector>
+import <vector>;
+//#include <vector> 不能使用include,会有重复，因为模块声明单元已经import。但是可以使用import
 
 //can't use import and #include both
 //模块实现后不能使用import 和#include
